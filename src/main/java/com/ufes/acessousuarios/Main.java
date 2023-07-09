4e10d263917f0b439a67ea27d3b29a3555d52250
Guilherme Oliveira <guilherme.oliveira@itixti.com.br>
2023-07-09 18:54:33 -0300

    feat: Initial commit

---
+++ src/main/java/com/ufes/acessousuarios/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ufes.acessousuarios;
+
+/**
+ *
+ * @author guiro
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
